dc59c2e14061c27f6d88a6ae543e8c0236de0e23
Stefan Reichert <reichert@qu-int.com>
2017-02-08 16:28:14 +0100

    Updated Off-Canvas templates for Foundation 6.3

--- footer.php
+++ footer.php
@@ -1,28 +1,27 @@
-						<footer class="footer" role="contentinfo">
-							<section id="inner-footer" class="row">
-								
-								<div class="small-12 columns">
-									<nav role="navigation">
-			    						<?php sr_footer_menu(); ?>
-			    					</nav>
-			    				</div>
-								
-								<?php if ( is_active_sidebar( 'sr-footer' ) ) : ?>
-									<div id="sidebar-sr-footer" class="small-12 columns">
-										<?php dynamic_sidebar( 'sr-footer' ); ?>
-									</div>
-								<?php endif; ?>
-								
-								<div class="small-12 columns">
-									<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>.</p>
+					<footer class="footer" role="contentinfo">
+						<section id="inner-footer" class="row">
+							
+							<div class="small-12 columns">
+								<nav role="navigation">
+		    						<?php sr_footer_menu(); ?>
+		    					</nav>
+		    				</div>
+							
+							<?php if ( is_active_sidebar( 'sr-footer' ) ) : ?>
+								<div id="sidebar-sr-footer" class="small-12 columns">
+									<?php dynamic_sidebar( 'sr-footer' ); ?>
 								</div>
-								
-							</section> <!-- end #inner-footer -->
-						</footer> <!-- end .footer -->
-					
-					</section> <!-- end .main-content -->
-				</div>  <!-- end .off-canvas-content -->
-			</div> <!-- end .off-canvas-wrapper-inner -->
+							<?php endif; ?>
+							
+							<div class="small-12 columns">
+								<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>.</p>
+							</div>
+							
+						</section> <!-- end #inner-footer -->
+					</footer> <!-- end .footer -->
+				
+				</section> <!-- end .main-content -->
+			</div>  <!-- end .off-canvas-content -->
 		</div> <!-- end .off-canvas-wrapper -->
 		<?php wp_footer(); ?>
 	</body>
